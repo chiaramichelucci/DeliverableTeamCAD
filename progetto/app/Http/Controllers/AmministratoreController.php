151bd874648868c593d4dbeec5b0faee564489b4
StratulatDragos <58547406+StratulatDragos@users.noreply.github.com>
2020-01-17 19:36:07 +0100

    Progetto in laravel

---
+++ progetto/app/Http/Controllers/AmministratoreController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AmministratoreController extends Controller
+{
+    //Controller per la gestione delle richieste provenienti da amministratori
+
+    public function stampaLezione($id)
+    {
+        
+    }
+
+    public function addLezione()
+    {
+        
+    }
+
+    public function varLezione()
+    {
+        
+    }
+}
